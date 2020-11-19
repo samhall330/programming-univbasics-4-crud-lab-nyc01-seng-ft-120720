2576ddc6e70d0722e25497a63b97c7ab09b8c2d7
Sam Hall <mann.samantha11@gmail.com>
2020-11-19 02:25:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,8 @@ array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-
+array = ["cats", "dogs", "pigs", "sheep"]
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
